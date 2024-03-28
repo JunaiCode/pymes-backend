9bae15307bd87dd1c1155005139aa0e8ec5bf6fa
JunaiCode <juanpalta24@hotmail.com>
2024-03-28 16:39:29 -0500

    first commit

---
+++ src/main/java/com/pdg/pymesbackend/PymesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pdg.pymesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdg/pymesbackend/PymesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdg.pymesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
