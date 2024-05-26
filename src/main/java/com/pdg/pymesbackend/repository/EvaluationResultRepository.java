71989da6f58e14972b6b8242432869e53fa612ad
Danilo Erazo <erazodanilo1218@gmail.com>
2024-05-25 20:15:41 -0500

    n-th try to delete evaluationResults

--- src/main/java/com/pdg/pymesbackend/repository/EvaluationResultRepository.java
+++ src/main/java/com/pdg/pymesbackend/repository/EvaluationResultRepository.java
@@ -2,11 +2,13 @@ package com.pdg.pymesbackend.repository;
 
 import com.pdg.pymesbackend.model.EvaluationResult;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface EvaluationResultRepository extends MongoRepository<EvaluationResult, String>{
 
+    @Query(value = "{ '_id' : { $in: ?0 } }", delete = true)
     void deleteAllByEvaluationResultIdIn(List<String> evaluationResultId);
 
 }
