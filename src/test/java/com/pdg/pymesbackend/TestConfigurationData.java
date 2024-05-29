bb6314d6501ccf83f8c033d38d34b77b3d0a9fa2
JunaiCode <juanpalta24@hotmail.com>
2024-05-29 13:28:38 -0500

    edit Ids TestConfigurationData

--- src/test/java/com/pdg/pymesbackend/TestConfigurationData.java
+++ src/test/java/com/pdg/pymesbackend/TestConfigurationData.java
@@ -31,8 +31,10 @@ public class TestConfigurationData {
         CompanyType companyType = CompanyType.builder().companyTypeId("MICRO").name("Micro Empresa").description("Menos de 10 empleados").build();
         Company company = Company.builder().companyId("companyId").companyType(companyType).employees(20).address("Carrera24").name("Emcali").economicSectorId("4").cityId("1").nit("2032030345").legalRep("Juan")
                 .legalRepEmail("juan@hotmail.com").legalRepTel("1234567892").tel("9876543210").password("123").creationDate(LocalDateTime.now()).evaluations(new ArrayList<String>()).build();
+        ArrayList<String> evaluationsCompany2 = new ArrayList<>();
+        evaluationsCompany2.add("Evaluation1Id");
         Company company2 = Company.builder().companyId("companyId2").companyType(companyType).employees(20).address("Carrera25").name("Alkosto").economicSectorId("4").cityId("1").nit("2032030345").legalRep("Pedro")
-                .legalRepEmail("pedro@hotmail.com").legalRepTel("1234567892").tel("9876543210").password("123").creationDate(LocalDateTime.now()).evaluations(new ArrayList<String>()).build();
+                .legalRepEmail("pedro@hotmail.com").legalRepTel("1234567892").tel("9876543210").password("123").creationDate(LocalDateTime.now()).evaluations(evaluationsCompany2).build();
         Level level1Tecnologia = Level.builder().levelId("level1TechId").description("Level 1 Tecnologia").name("Nivel 1").value(1).questions(new ArrayList<String>()).build();
         Level level2Tecnologia = Level.builder().levelId("level2TechId").description("Level 2 Tecnologia").name("Nivel 2").value(2).questions(new ArrayList<String>()).build();
         Level level1Procesos = Level.builder().levelId("level1ProcessId").description("Level 1 Procesos").name("Nivel 1").value(1).questions(new ArrayList<String>()).build();
@@ -48,20 +50,20 @@ public class TestConfigurationData {
         Tag tagProcesos = Tag.builder().tagId("tagProcessId").description("Instrumentos").name("Instrumentos").dimensionId(dimension2.getDimensionId()).build();
 
         /*TECNOLOGIA Nivel 1*/
-        Step stepQuestionTecnologia1 = Step.builder().stepId(UUID.randomUUID().toString()).order(1).description("Primer Paso").build();
-        Step stepQuestionTecnologia2 = Step.builder().stepId(UUID.randomUUID().toString()).order(2).description("Segundo Paso").build();
-        Step stepQuestionTecnologia3 = Step.builder().stepId(UUID.randomUUID().toString()).order(3).description("Tercer Paso").build();
-        Step stepQuestionTecnologia4 = Step.builder().stepId(UUID.randomUUID().toString()).order(4).description("Cuarto Paso").build();
+        Step stepQuestionTecnologia1 = Step.builder().stepId("stepQuestionTecnologia1").order(1).description("Primer Paso").build();
+        Step stepQuestionTecnologia2 = Step.builder().stepId("stepQuestionTecnologia2").order(2).description("Segundo Paso").build();
+        Step stepQuestionTecnologia3 = Step.builder().stepId("stepQuestionTecnologia3").order(3).description("Tercer Paso").build();
+        Step stepQuestionTecnologia4 = Step.builder().stepId("stepQuestionTecnologia4").order(4).description("Cuarto Paso").build();
         ArrayList<Step> stepsTecnologia1 = new ArrayList<>();
         stepsTecnologia1.add(stepQuestionTecnologia1);
         stepsTecnologia1.add(stepQuestionTecnologia2);
         stepsTecnologia1.add(stepQuestionTecnologia3);
         stepsTecnologia1.add(stepQuestionTecnologia4);
-        Recommendation recommendationQuestionTecnologia1 = Recommendation.builder().recommendationId(UUID.randomUUID().toString()).title("Titulo pregunta 1 tecnologia").questionId("questionTecnologia1Id").description("Tecnologia Q1").steps(stepsTecnologia1).build();
-        Option optionTecnologia1 = Option.builder().optionId(UUID.randomUUID().toString()).value(10).description("Opcion 1").build();
-        Option optionTecnologia2 = Option.builder().optionId(UUID.randomUUID().toString()).value(-1).description("Opcion 2").build();
-        Option optionTecnologia3 = Option.builder().optionId(UUID.randomUUID().toString()).value(-2).description("Opcion 3").build();
-        Option optionTecnologia4 = Option.builder().optionId(UUID.randomUUID().toString()).value(-3).description("Opcion 4").build();
+        Recommendation recommendationQuestionTecnologia1 = Recommendation.builder().recommendationId("recommendationQuestionTecnologia1").title("Titulo pregunta 1 tecnologia").questionId("questionTecnologia1Id").description("Tecnologia Q1").steps(stepsTecnologia1).build();
+        Option optionTecnologia1 = Option.builder().optionId("optionTecnologia1").value(10).description("Opcion 1").build();
+        Option optionTecnologia2 = Option.builder().optionId("optionTecnologia2").value(-1).description("Opcion 2").build();
+        Option optionTecnologia3 = Option.builder().optionId("optionTecnologia3").value(-2).description("Opcion 3").build();
+        Option optionTecnologia4 = Option.builder().optionId("optionTecnologia4").value(-3).description("Opcion 4").build();
         List<Option> optionsQuestionTecnologia1 = new ArrayList<Option>();
         optionsQuestionTecnologia1.add(optionTecnologia1);
         optionsQuestionTecnologia1.add(optionTecnologia2);
@@ -74,25 +76,25 @@ public class TestConfigurationData {
         levelsTecnologia.add(level1Tecnologia);
 
         /*TECNOLOGIA Nivel 2*/
-        Step stepQuestionTecnologiaQ2_1 = Step.builder().stepId(UUID.randomUUID().toString()).order(1).description("Primer Paso").build();
-        Step stepQuestionTecnologiaQ2_2 = Step.builder().stepId(UUID.randomUUID().toString()).order(2).description("Segundo Paso").build();
-        Step stepQuestionTecnologiaQ2_3 = Step.builder().stepId(UUID.randomUUID().toString()).order(3).description("Tercer Paso").build();
-        Step stepQuestionTecnologiaQ2_4 = Step.builder().stepId(UUID.randomUUID().toString()).order(4).description("Cuarto Paso").build();
+        Step stepQuestionTecnologiaQ2_1 = Step.builder().stepId("stepQuestionTecnologiaQ2_1").order(1).description("Primer Paso").build();
+        Step stepQuestionTecnologiaQ2_2 = Step.builder().stepId("stepQuestionTecnologiaQ2_2").order(2).description("Segundo Paso").build();
+        Step stepQuestionTecnologiaQ2_3 = Step.builder().stepId("stepQuestionTecnologiaQ2_3").order(3).description("Tercer Paso").build();
+        Step stepQuestionTecnologiaQ2_4 = Step.builder().stepId("stepQuestionTecnologiaQ2_4").order(4).description("Cuarto Paso").build();
         ArrayList<Step> stepsTecnologia2 = new ArrayList<>();
         stepsTecnologia2.add(stepQuestionTecnologiaQ2_1);
         stepsTecnologia2.add(stepQuestionTecnologiaQ2_2);
         stepsTecnologia2.add(stepQuestionTecnologiaQ2_3);
         stepsTecnologia2.add(stepQuestionTecnologiaQ2_4);
-        Recommendation recommendationQuestionTecnologia2 = Recommendation.builder().recommendationId(UUID.randomUUID().toString()).title("Titulo pregunta 2 tecnologia").questionId("questionTecnologia2Id").description("Tecnologia Q2").steps(stepsTecnologia2).build();
-        Option optionTecnologiaQ2_1 = Option.builder().optionId(UUID.randomUUID().toString()).value(10).description("Opcion 1").build();
-        Option optionTecnologiaQ2_2 = Option.builder().optionId(UUID.randomUUID().toString()).value(-1).description("Opcion 2").build();
-        Option optionTecnologiaQ2_3 = Option.builder().optionId(UUID.randomUUID().toString()).value(-2).description("Opcion 3").build();
-        Option optionTecnologiaQ2_4 = Option.builder().optionId(UUID.randomUUID().toString()).value(-3).description("Opcion 4").build();
+        Recommendation recommendationQuestionTecnologia2 = Recommendation.builder().recommendationId("recommendationQuestionTecnologia2").title("Titulo pregunta 2 tecnologia").questionId("questionTecnologia2Id").description("Tecnologia Q2").steps(stepsTecnologia2).build();
+        Option optionTecnologiaQ2_1 = Option.builder().optionId("optionTecnologiaQ2_1").value(10).description("Opcion 1").build();
+        Option optionTecnologiaQ2_2 = Option.builder().optionId("optionTecnologiaQ2_2").value(-1).description("Opcion 2").build();
+        Option optionTecnologiaQ2_3 = Option.builder().optionId("optionTecnologiaQ2_3").value(-2).description("Opcion 3").build();
+        Option optionTecnologiaQ2_4 = Option.builder().optionId("optionTecnologiaQ2_4").value(-3).description("Opcion 4").build();
         List<Option> optionsQuestionTecnologia2 = new ArrayList<Option>();
-        optionsQuestionTecnologia1.add(optionTecnologiaQ2_1);
-        optionsQuestionTecnologia1.add(optionTecnologiaQ2_2);
-        optionsQuestionTecnologia1.add(optionTecnologiaQ2_3);
-        optionsQuestionTecnologia1.add(optionTecnologiaQ2_4);
+        optionsQuestionTecnologia2.add(optionTecnologiaQ2_1);
+        optionsQuestionTecnologia2.add(optionTecnologiaQ2_2);
+        optionsQuestionTecnologia2.add(optionTecnologiaQ2_3);
+        optionsQuestionTecnologia2.add(optionTecnologiaQ2_4);
         Question questionTecnologia2 = Question.builder().questionId("questionTecnologia2Id").question("Pregunta 2 tecnologia").tagId(tagTecnologico.getTagId()).companyTypeId(companyType.getCompanyTypeId()).scorePositive(10).dimensionId(dimension1.getDimensionId()).weight(1.0).options(optionsQuestionTecnologia2).recommendation(recommendationQuestionTecnologia2).build();
         ArrayList<String>questionsTechLevel2 = new ArrayList<>();
         questionsTechLevel2.add(questionTecnologia2.getQuestionId());
@@ -100,25 +102,25 @@ public class TestConfigurationData {
         levelsTecnologia.add(level2Tecnologia);
 
         /*PROCESOS*/
-        Step stepQuestionProcesos1 = Step.builder().stepId(UUID.randomUUID().toString()).order(1).description("Primer Paso").build();
-        Step stepQuestionProcesos2 = Step.builder().stepId(UUID.randomUUID().toString()).order(2).description("Segundo Paso").build();
-        Step stepQuestionProcesos3 = Step.builder().stepId(UUID.randomUUID().toString()).order(3).description("Tercer Paso").build();
-        Step stepQuestionProcesos4 = Step.builder().stepId(UUID.randomUUID().toString()).order(4).description("Cuarto Paso").build();
+        Step stepQuestionProcesos1 = Step.builder().stepId("stepQuestionProcesos1").order(1).description("Primer Paso").build();
+        Step stepQuestionProcesos2 = Step.builder().stepId("stepQuestionProcesos2").order(2).description("Segundo Paso").build();
+        Step stepQuestionProcesos3 = Step.builder().stepId("stepQuestionProcesos3").order(3).description("Tercer Paso").build();
+        Step stepQuestionProcesos4 = Step.builder().stepId("stepQuestionProcesos4").order(4).description("Cuarto Paso").build();
         ArrayList<Step> stepsProcesos1 = new ArrayList<>();
         stepsProcesos1.add(stepQuestionProcesos1);
         stepsProcesos1.add(stepQuestionProcesos2);
         stepsProcesos1.add(stepQuestionProcesos3);
         stepsProcesos1.add(stepQuestionProcesos4);
-        Recommendation recommendationQuestionProcesos1 = Recommendation.builder().recommendationId(UUID.randomUUID().toString()).title("Titulo pregunta 1 Procesos").questionId("questionProcess1Id").description("Procesos Q1").steps(stepsProcesos1).build();
-        Option optionProcesos1 = Option.builder().optionId(UUID.randomUUID().toString()).value(10).description("Opcion 1").build();
-        Option optionProcesos2 = Option.builder().optionId(UUID.randomUUID().toString()).value(-1).description("Opcion 2").build();
-        Option optionProcesos3 = Option.builder().optionId(UUID.randomUUID().toString()).value(-2).description("Opcion 3").build();
-        Option optionProcesos4 = Option.builder().optionId(UUID.randomUUID().toString()).value(-3).description("Opcion 4").build();
+        Recommendation recommendationQuestionProcesos1 = Recommendation.builder().recommendationId("recommendationQuestionProcesos1").title("Titulo pregunta 1 Procesos").questionId("questionProcess1Id").description("Procesos Q1").steps(stepsProcesos1).build();
+        Option optionProcesos1 = Option.builder().optionId("optionProcesos1").value(10).description("Opcion 1").build();
+        Option optionProcesos2 = Option.builder().optionId("optionProcesos2").value(-1).description("Opcion 2").build();
+        Option optionProcesos3 = Option.builder().optionId("optionProcesos3").value(-2).description("Opcion 3").build();
+        Option optionProcesos4 = Option.builder().optionId("optionProcesos4").value(-3).description("Opcion 4").build();
         List<Option> optionsQuestionProcesos1 = new ArrayList<Option>();
-        optionsQuestionTecnologia1.add(optionProcesos1);
-        optionsQuestionTecnologia1.add(optionProcesos2);
-        optionsQuestionTecnologia1.add(optionProcesos3);
-        optionsQuestionTecnologia1.add(optionProcesos4);
+        optionsQuestionProcesos1.add(optionProcesos1);
+        optionsQuestionProcesos1.add(optionProcesos2);
+        optionsQuestionProcesos1.add(optionProcesos3);
+        optionsQuestionProcesos1.add(optionProcesos4);
         Question questionProcesos1 = Question.builder().questionId("questionProcess1Id").question("Pregunta 1 Procesos").tagId(tagProcesos.getTagId()).companyTypeId(companyType.getCompanyTypeId()).scorePositive(10).dimensionId(dimension2.getDimensionId()).weight(1.0).options(optionsQuestionProcesos1).recommendation(recommendationQuestionProcesos1).build();
         ArrayList<String>questionsProcessLevel1 = new ArrayList<>();
         questionsProcessLevel1.add(questionProcesos1.getQuestionId());
@@ -126,20 +128,20 @@ public class TestConfigurationData {
         levelsProcesos.add(level1Procesos);
 
         /*PROCESOS Nivel 2*/
-        Step stepQuestionProcesosQ2_1 = Step.builder().stepId(UUID.randomUUID().toString()).order(1).description("Primer Paso").build();
-        Step stepQuestionProcesosQ2_2 = Step.builder().stepId(UUID.randomUUID().toString()).order(2).description("Segundo Paso").build();
-        Step stepQuestionProcesosQ2_3 = Step.builder().stepId(UUID.randomUUID().toString()).order(3).description("Tercer Paso").build();
-        Step stepQuestionProcesosQ2_4 = Step.builder().stepId(UUID.randomUUID().toString()).order(4).description("Cuarto Paso").build();
+        Step stepQuestionProcesosQ2_1 = Step.builder().stepId("stepQuestionProcesosQ2_1").order(1).description("Primer Paso").build();
+        Step stepQuestionProcesosQ2_2 = Step.builder().stepId("stepQuestionProcesosQ2_2").order(2).description("Segundo Paso").build();
+        Step stepQuestionProcesosQ2_3 = Step.builder().stepId("stepQuestionProcesosQ2_3").order(3).description("Tercer Paso").build();
+        Step stepQuestionProcesosQ2_4 = Step.builder().stepId("stepQuestionProcesosQ2_4").order(4).description("Cuarto Paso").build();
         ArrayList<Step> stepsProcesos2 = new ArrayList<>();
         stepsProcesos2.add(stepQuestionProcesosQ2_1);
         stepsProcesos2.add(stepQuestionProcesosQ2_2);
         stepsProcesos2.add(stepQuestionProcesosQ2_3);
         stepsProcesos2.add(stepQuestionProcesosQ2_4);
-        Recommendation recommendationQuestionProcesos2 = Recommendation.builder().recommendationId(UUID.randomUUID().toString()).title("Titulo pregunta 2 Procesos").questionId("questionProcess2").description("Procesos Q2").steps(stepsProcesos2).build();
-        Option optionProcesosQ2_1 = Option.builder().optionId(UUID.randomUUID().toString()).value(10).description("Opcion 1").build();
-        Option optionProcesosQ2_2 = Option.builder().optionId(UUID.randomUUID().toString()).value(-1).description("Opcion 2").build();
-        Option optionProcesosQ2_3 = Option.builder().optionId(UUID.randomUUID().toString()).value(-2).description("Opcion 3").build();
-        Option optionProcesosQ2_4 = Option.builder().optionId(UUID.randomUUID().toString()).value(-3).description("Opcion 4").build();
+        Recommendation recommendationQuestionProcesos2 = Recommendation.builder().recommendationId("recommendationQuestionProcesos2").title("Titulo pregunta 2 Procesos").questionId("questionProcess2").description("Procesos Q2").steps(stepsProcesos2).build();
+        Option optionProcesosQ2_1 = Option.builder().optionId("optionProcesosQ2_1").value(10).description("Opcion 1").build();
+        Option optionProcesosQ2_2 = Option.builder().optionId("optionProcesosQ2_2").value(-1).description("Opcion 2").build();
+        Option optionProcesosQ2_3 = Option.builder().optionId("optionProcesosQ2_3").value(-2).description("Opcion 3").build();
+        Option optionProcesosQ2_4 = Option.builder().optionId("optionProcesosQ2_4").value(-3).description("Opcion 4").build();
         List<Option> optionsQuestionProcesos2 = new ArrayList<Option>();
         optionsQuestionProcesos2.add(optionProcesosQ2_1);
         optionsQuestionProcesos2.add(optionProcesosQ2_2);
@@ -163,7 +165,9 @@ public class TestConfigurationData {
 
         /*EVALUATION*/
         ActionPlan actionPlan = ActionPlan.builder().actionPlanId("ActionPlan1Id").recommendationActionPlans(new ArrayList<>()).recommendations(new ArrayList<>()).start(LocalDateTime.now()).end(LocalDateTime.now()).build();
-        Evaluation evaluation = Evaluation.builder().evaluationId("Evaluation1Id").date(LocalDateTime.now()).dimensionResults(new ArrayList<>()).questionResults(new ArrayList<>()).completed(true).actionPlanId(actionPlan.getActionPlanId()).build();
+        ArrayList<String> questionResult = new ArrayList<>();
+        questionResult.add("");
+        Evaluation evaluation = Evaluation.builder().evaluationId("Evaluation1Id").date(LocalDateTime.now()).dimensionResults(new ArrayList<>()).questionResults(questionResult).completed(false).actionPlanId(actionPlan.getActionPlanId()).build();
 
         return args -> {
             admins.deleteAll();
--- src/test/java/com/pdg/pymesbackend/integration/controller/EvaluationControllerTest.java
+++ src/test/java/com/pdg/pymesbackend/integration/controller/EvaluationControllerTest.java
@@ -2,13 +2,18 @@ package com.pdg.pymesbackend.integration.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pdg.pymesbackend.TestConfigurationData;
+import com.pdg.pymesbackend.dto.ModelDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,19 +26,29 @@ public class EvaluationControllerTest {
     @Autowired
     ObjectMapper mapper;
 
+    String companyId = "companyId2";
+    String evaluationId = "Evaluation1Id";
+
+    String Version1Id = "Version1Id";
     @Test
     public void createEvaluation() throws Exception{
-
+        var result = mvc.perform(MockMvcRequestBuilders.post("/evaluation/add/{companyId}",companyId).contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
     }
 
     @Test
     public void finishEvaluation() throws Exception{
-
+        var result = mvc.perform(MockMvcRequestBuilders.post("/evaluation/finish/{evaluationId}/version/{versionId}",evaluationId,Version1Id).contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
     }
 
     @Test
     public void getEvaluation() throws Exception{
-
+        var result = mvc.perform(MockMvcRequestBuilders.get("/evaluation/{evaluationId}",evaluationId).contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
     }
 
     @Test
